d96cf3c7de82886d0cd8053706f8df5bd0119727
Ольга Абрамова <agnysky@yandex.ru>
2025-11-27 15:05:21 +0300

    Новая ветка и текст КР1

---
+++ NewClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CosoleProjectforGithub
+{
+    internal class NewClass
+    {
+        public NewClass() 
+        {
+            Console.WriteLine("Команда разработчиков 1");
+        }
+      
+    }
+}
